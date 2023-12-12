b595fa3dd5becd1d59edb08cc2ee22792f272790
AlexTouchedGrass <144457672+AlexTouchedGrass@users.noreply.github.com>
2023-12-11 20:07:12 -0500

    Update Main.java
    BOOM

--- src/Main.java
+++ src/Main.java
@@ -1,106 +1,68 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int score = 0;
         boolean done = false;
 
         do {
+            int score = 0;
             System.out.println("Please enter a password you would like to have checked.");
-            boolean need12Length = get12LengthString(scan);
-            if (need12Length) {
+
+            if (get12LengthString(scan)) {
                 score++;
             }
 
-            if () {
-
+            if (containsUppercase(scan)) {
+                score++;
             }
 
-            if () {
-
+            if (containsLowercase(scan)) {
+                score++;
             }
 
-            if () {
-
+            if (containsNumber(scan)) {
+                score++;
             }
 
-            if () {
-
+            if (containsSpecialCharacter(scan)) {
+                score++;
             }
 
-            if (score > 4) {
-                System.out.println("Your score is " + score + ".");
-                System.out.println("Great Score!");
+            System.out.println("Your score is " + score + ".");
+
+            if (score >= 4) {
+                System.out.println("Great Score! Your password is strong.");
             } else if (score == 3) {
-                System.out.println("Your score is " + score + ".");
-                System.out.println("Good Score!");
-            } else if (score == 2) {
-                System.out.println("Your score is " + score + ".");
-                System.out.println("Okay Score!");
-            } else if (score == 1) {
-                System.out.println("Your score is " + score + ".");
-                System.out.println("Poor Score!");
+                System.out.println("Good Score! Your password is of medium strength.");
             } else {
-                System.out.println("Your score is " + score + ".");
-                System.out.println("You Failed!");
+                System.out.println("Weak Score! Your password is weak.");
             }
 
             boolean doneYN = false;
             do {
-                boolean YN = getYNConfirm(scan,"Would you like to play again? (Y/N)");
+                boolean YN = getYNConfirm(scan, "Would you like to check another password? (Y/N)");
 
                 if (!YN) {
                     doneYN = true;
-                } else {
                     done = true;
+                } else {
                     doneYN = true;
                 }
-            } while (!doneYN); //I know this is seen as unnecessary but I feel safer with it and I don't think it impacts the code.
+            } while (!doneYN);
+
         } while (!done);
     }
 
-
-
-
-
-    //FUNCTIONS
-    public static boolean get12LengthString(Scanner in){
+    // FUNCTIONS
+    public static boolean get12LengthString(Scanner in) {
         boolean done = false;
         String input;
 
         System.out.println();
-        do{
+        do {
             input = in.nextLine();
-            if(input.length() > 12){
+            if (input.length() >= 12) {
                 done = true;
-            }
-            // in.nextLine(); // could be use for buffer
-        }while(!done);
-        return done;
-    }
-
-    public static boolean getYNConfirm(Scanner in, String prompt){
-        boolean done = false;
-        String input;
-        boolean YN = false;
-
-        System.out.println(prompt);
-        do{
-            input = in.nextLine();
-            if(input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y")){
-                System.out.println("You will continue.");
-                input = "YES";
-                done = true;
-                YN = true;
-            } else if (input.equalsIgnoreCase("no") || input.equalsIgnoreCase("n")) {
-                System.out.println("You will not continue.");
-                input = "NO";
-                done = true;
-            }
-            //in.nextLine(); //not needed rn.
-        }while(!done);
-        return YN;
-    }
-}
-
+     
